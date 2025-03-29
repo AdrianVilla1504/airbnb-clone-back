00b0e9472a44b239cc3dfb6448655e93f0b9c46f
AdrianVilla1504 <adriancvila@gmail.com>
2025-03-29 12:44:26 -0500

    changes: change cors policy

--- config/express.js
+++ config/express.js
@@ -4,6 +4,12 @@ const morgan = require('morgan');
 
 function configExpress(app) {
   app.use(cors());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
   app.use(express.json());
   app.use(morgan('dev'));
 }
